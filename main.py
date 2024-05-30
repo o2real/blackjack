3d71cab0450e6061a1f8a668dac8a27e51375eb2
Kingcode <136673119+o2real@users.noreply.github.com>
2024-05-30 04:58:25 +0000

    블랙잭1

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
-print("hello")
+import random
 
-그래서 언제할건데;
+def deal_card():
+  cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+  card = random.choice(cards)
+  return card
+
+user_cards = []
+computer_cards = []
+
+for _ in range(2):
+  new_card = deal_card()
+  user_cards.append(new_card)
